28cef96dbd9b6cbf703becafd1a61a1eceb3ffa9
Thomas75019 <tlarousse3@gmail.com>
2022-04-04 21:14:59 +0200

    creates UserClass

--- library/Interfaces/UserInterface.php
+++ library/Interfaces/UserInterface.php
@@ -6,7 +6,6 @@ interface UserInterface
 {
     public function getObject(): string;
     public function getId(): string;
-    public function setId(string $id): void;
     public function setType(string $type): void;
     public function getType(): string;
     public function setName(string $name): void;
