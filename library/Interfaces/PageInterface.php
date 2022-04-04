748644ea55cfac7a94b1f9577d55da83d0224ee1
Thomas75019 <tlarousse3@gmail.com>
2022-04-04 20:52:31 +0200

    Creates page interface

--- library/Interfaces/PageInterface.php
+++ library/Interfaces/PageInterface.php
@@ -2,35 +2,36 @@
 
 namespace Library\Interfaces;
 
+use DateTime;
+
 interface PageInterface
 {
     public function getObject(): string;
 
-    public function setPageId(): string;
+    public function setPageId(string $pageId): string;
     public function getPageId(): string;
 
-    public function setCreatedTime(): string;
-    public function getCreatedTime(): string;
+    public function getCreatedTime(): DateTime;
 
-    public function setLastEditedTime(): string;
-    public function getLastEditedTime(): string;
+    public function setLastEditedTime(DateTime $date): void;
+    public function getLastEditedTime(): DateTime;
 
-    public function setLastEditedBy(): Object;
-    public function getLastEditedBy(): Object;
+    public function setLastEditedBy(UserInterface $user): void;
+    public function getLastEditedBy(): UserInterface;
 
-    public function setArchived(): bool;
+    public function setArchived(bool $archived): void;
     public function getArchived(): bool;
 
-    public function setIcon(): Object;
-    public function getIcon(): Object;
+    public function setIcon(EmojiInterface $emoji): void;
+    public function getIcon(): EmojiInterface;
 
-    public function setProperties(): Object;
-    public function getProperties(): Object;
+    public function setProperties(PropertiesObjectInterface $properties): void;
+    public function getProperties(): PropertiesObjectInterface;
 
-    public function setParent(): Object;
-    public function getParent(): Object;
+    public function setParent(ParentInterface $prarent): void;
+    public function getParent(): ParentInterface;
 
     
-    public function setUrl(): string;
+    public function setUrl(string $url): void;
     public function getUrl(): string;
 }
